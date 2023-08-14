7a2a8af0afa662d3f142960d6f6659ff699e9ff7
maxwalves <maxwilliamalves@gmail.com>
2023-08-14 12:54:23 -0300

    Correções.

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -175,7 +175,7 @@
                         "info": "Mostrando página _PAGE_ de _PAGES_",
                         "infoEmpty": "Nenhum registro disponível",
                         "infoFiltered": "(filtrado de _MAX_ registros no total)",
-                        "search": "Procure uma AV"
+                        "search": "Procure um usuário:"
                     }
                 });
         });
